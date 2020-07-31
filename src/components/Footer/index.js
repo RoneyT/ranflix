dd4050f2e013f9dffa0595b3c80154669de8168a
roneyt <roneyfernandes1@gmail.com>
2020-07-30 21:01:36 -0300

    Alteracao logo inferior

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,10 +5,10 @@ function Footer() {
   return (
     <FooterBase>
       <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+        <img src="https://ranflix.vercel.app/static/media/Logo.4064dcc1.png" alt="Logo Alura" />
       </a>
       <p>
-        Orgulhosamente criado por Roney Fernandes durante a
+        Orgulhosamente criado por Roney "Ranfis" Fernandes durante a
         {' '}
         <a href="https://www.alura.com.br/">
           Imersão React da Alura
